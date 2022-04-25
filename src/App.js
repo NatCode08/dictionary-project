d4064c9d0b1af37f3bb5d118a2f49f59c8b0fc3f
NatCode08 <natortizm@gmail.com>
2022-04-26 00:19:58 +0200

    Added a footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <div className="container">Dictionary</div>
       <Dictionary />
+
+      <footer> Coded by Natalia Ortiz </footer>
     </div>
   );
 }
