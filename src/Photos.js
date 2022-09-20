114eb7bcbac2187c5913b9f9f81e67abe26602ae
NatCode08 <natortizm@gmail.com>
2022-09-21 00:06:15 +0200

    added a grid for the photos

--- src/Photos.js
+++ src/Photos.js
@@ -3,19 +3,25 @@ import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    console.log(props.photos);
     return (
-      <section className="photos">
-        <div className="row">
-          {props.photos.map(function (photo, index) {
-            return (
-              <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
-                </a>
-              </div>
-            );
-          })}
+      <section className="Photos">
+        <div className="container">
+          <div className="row justifiy-content-evenly">
+            {props.photos.map(function (photo, index) {
+              console.log(photo);
+              return (
+                <div className="col-3" key={index}>
+                  <a href={photo.src.original} target="_blank" rel="noreferrer">
+                    <img
+                      src={photo.src.landscape}
+                      className="img-fluid"
+                      alt="result"
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     );
